7909cf6af98ec2a295981f1d3231e048a8685ec5
charlesmicheni <charlesmugambi100@gmail.com>
2020-10-07 02:45:30 +0300

    final work commits

---
+++ github-serch/src/app/constants.spec.ts
@@ -0,0 +1,6 @@
+import { Constants } from './constants';
+  describe('Constants', () => {
+  it('should create an instance', () => {
+    expect(new Constants()).toBeTruthy();
+  });
+});
---
+++ github-serch/src/app/service-request.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+import { User } from "./user";
+import { Repository } from "./repository-class";
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+@Injectable({
+  providedIn: "root"
+})
+export class ServiceRequestService {
+  user: User;
+  repos: Repository;
+  private username: string;
+  items;
+  constructor(private http: HttpClient) {
+    console.log("You can now access the service");
+    this.username = "mwikia bundi";
+    this.user = new User("");
+    this.repos = new Repository("", 0, 0, 0, new Date(), "", "");
+  }
+  getProfileInfo(username) {
+    console.log(username);
+    interface ApiResponse {
+      login: string;
+      public_repos: number;
+      followers: number;
+      following: number;
+      created_at: Date;
+      html_url: string;
+      avatar_url: string;
+    }
+    const promise = new Promise((resolve, reject) => {
+      this.http
+        .get<ApiResponse>(
+          "https://api.github.com/users/" +
+            username +
+            "?access_token=" +
+            "3d49dda6ba737440f98ff3f461496b1242ad75b2"
+        )
+        .subscribe(data => {
+          this.repos.login = data.login;
+          this.repos.public_repos = data.public_repos;
+          this.repos.followers = data.followers;
+          this.repos.created_at = data.created_at;
+          this.repos.html_url = data.html_url;
+          this.repos.avatar_url = data.avatar_url;
+          resolve();
+        });
+    });
+    return promise;
+  }
+  getRepoInfo(username) {
+    interface ApiResponse {
+      login: string;
+      public_repos: number;
+      followers: number;
+      following: number;
+      created_at: Date;
+      html_url: string;
+      avatar_url: string;
+    }
+    this.http
+      .get<ApiResponse>(
+        "https://api.github.com/users/" +
+          username +
+          "?access_token=" +
+          " 3d49dda6ba737440f98ff3f461496b1242ad75b2"
+      )
+      .subscribe(data => {
+        this.items = Response;
+      });
+  }
+}
